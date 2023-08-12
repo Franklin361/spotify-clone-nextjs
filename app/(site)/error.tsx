1d1749f922d723b5e49052e08e3e0e75fc3e250f
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2023-08-11 20:52:40 -0600

    feat: add erros and loadings in each page

---
+++ app/(site)/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Box from '@/components/Box';
+
+const Error = () => {
+    return (
+        <Box className='h-full flex  items-center justify-center'>
+            <div className='text-neutral-400'>
+                Somenthing was wrong
+            </div>
+        </Box>
+    )
+}
+export default Error
